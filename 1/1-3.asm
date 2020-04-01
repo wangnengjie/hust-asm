20242f5683d7ffb240a768cca2c3f48bfcda690b
wangnengjie <751614701@qq.com>
2020-04-01 15:23:59 +0800

    1

---
+++ 1/1-3.asm
@@ -0,0 +1,29 @@
+.386
+STACK   SEGMENT  USE16 STACK
+        DB  200 DUP(0)
+STACK   ENDS
+DATA    SEGMENT USE16
+BUF1    DB 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+BUF2    DB 10 DUP(0)
+BUF3    DB 10 DUP(0)
+BUF4    DB 10 DUP(0)
+DATA    ENDS
+CODE    SEGMENT USE16
+        ASSUME CS: CODE, DS: DATA, SS: STACK
+START:  MOV AX, DATA
+        MOV DS, AX
+        MOV EBX, 0
+        MOV CX, 10
+LOPA:   MOV AL, BUF1[EBX]
+        MOV BUF2[EBX], AL
+        INC AL
+        MOV BUF3[EBX], AL
+        ADD AL, 3
+        MOV BUF4[EBX], AL
+        INC EBX
+        DEC CX
+        JNZ LOPA
+        MOV AH, 4CH
+        INT 21H
+CODE    ENDS
+        END START
